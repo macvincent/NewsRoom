4784dc43aefcddb77983fa38038e03452e5f4f8a
MacVincent Agha-Oko <macvincentaghaoko@gmail.com>
2019-08-08 21:07:04 -0400

    fix: Update styling for blog page

--- NewsRoom/settings.py
+++ NewsRoom/settings.py
@@ -135,3 +135,5 @@ django_heroku.settings(locals())
 import dj_database_url 
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+# Temp fix for password recovery
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
