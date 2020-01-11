192dfff60e0f9c98c2c7743347e29ae147c65a98
macvincent <macvincentaghaoko@gmail.com>
2020-01-11 17:27:52 -0500

    feat: Fix issue with url for retrieving trending analyses

--- blog/static/js/trending.js
+++ blog/static/js/trending.js
@@ -1,5 +1,5 @@
 function updateLatestStories(){
-    $.get('http://127.0.0.1:61690/latest/', data =>{
+    $.get('latest/', data =>{
         data.forEach(news => {
             $('#trendingStories').append(`<li  id="trendingArticles"><a href="blogchatroom/?news=${news.title}"><section>${news.title}</section></a></li>`);
         });
