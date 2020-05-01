5d2e489d4bfb88d5b0ce40e494ea5465f086a845
macvincent <macvincentaghaoko@gmail.com>
2020-05-01 13:46:55 -0400

    feat: Update blog stories

--- blog/static/js/trending.js
+++ blog/static/js/trending.js
@@ -6,6 +6,39 @@ function updateLatestStories(){
     });
 };
 
-// Update trending stories every minute
+// get new stories for blog
+let call_num = 1
+function updateBlog(){
+    $.get('update/', {call: call_num}).done(data => {
+        Object.values(data).forEach(news => {
+            $('.mainFeed').append(`<div  class="article">
+            <a href="{% url 'chatroom' %}?news=${news.title}">
+                <p class="blogText">
+                    ${news.title}
+                </p>
+                <img src="${news.image}" height="400" width="500"/><br>
+                <div class="comment blogText">
+                    ${news.post}
+                </div>
+            </a>
+        </div>`)
+        });
+        call_num++;
+        needUpdate();
+    });
+}
+// Update trending stories two minutes
 updateLatestStories();
-setTimeout(updateLatestStories, 60000);
+setTimeout(updateLatestStories, 120000);
+
+function needUpdate(){
+    $(window).on("scroll", e => {
+        var scrollHeight = $(document).height();
+        var scrollPosition = $(window).height() + $(window).scrollTop();
+        if ((scrollHeight - scrollPosition) < scrollHeight/4) {
+            $(window).off("scroll");
+            updateBlog();
+        }
+    });
+}
+needUpdate();
--- blog/urls.py
+++ blog/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('chatroom/', login_required(views.ChatRoom.as_view()), name = 'viewcomment'),
     path('profile/', login_required(views.ProfileView.as_view()), name = 'profile'),
     path('latest/', views.TrendingStories.as_view(), name = 'trending'),
+    path('update/', views.AsyncStories.as_view(), name = 'update_stories'),
 ]
